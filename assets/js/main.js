ac62f7cc108009ccd1e4fc8e7f66057ac12df5dc
manash-git123 <48446680+manash-git123@users.noreply.github.com>
2020-06-08 13:13:18 +0530

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,6 +4,31 @@
 * Author: BootstrapMade.com
 * License: https://bootstrapmade.com/license/
 */
+
+$('#myCarousel').carousel({
+  interval: 1000
+})
+
+$('.carousel .carousel-item').each(function() {
+  var minPerSlide = 4;
+  var next = $(this).next();
+  if (!next.length) {
+      next = $(this).siblings(':first');
+  }
+  next.children(':first-child').clone().appendTo($(this));
+
+  for (var i = 0; i < minPerSlide; i++) {
+      next = next.next();
+      if (!next.length) {
+          next = $(this).siblings(':first');
+      }
+
+      next.children(':first-child').clone().appendTo($(this));
+  }
+});
+
+
+
 !(function($) {
   "use strict";
 
